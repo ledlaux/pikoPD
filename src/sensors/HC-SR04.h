a213d11ecc86b4f87a6caf6aff1f07af1d0957d4
Vadims Maksimovs <ledlaux@gmail.com>
2026-04-26 20:47:55 +0300

    add distance sensor

---
+++ src/sensors/HC-SR04.h
@@ -0,0 +1,81 @@
+#if DISTANCE_SENSOR_ENABLED
+
+#pragma once
+
+#include "pico/stdlib.h"
+#include "hardware/pio.h"
+#include "hardware/timer.h"
+
+#ifdef DISTANCE_SENSOR_ENABLED
+    #include "distance_sensor.h"
+#endif
+
+#include <cmath>
+#include <cstdio> 
+
+namespace Pico {
+
+class SonicSensor {
+private:
+    float current_val = 0.0f;
+    int last_reported = -1;
+    DistanceSensor* hw = nullptr;
+    struct repeating_timer timer;
+
+    static bool timer_callback(struct repeating_timer *t) {
+        auto* self = static_cast<SonicSensor*>(t->user_data);
+        if (self->hw && !self->hw->is_sensing) {
+            self->hw->TriggerRead();
+        }
+        return true; 
+    }
+
+public:
+    void init(uint32_t trig, uint32_t echo, pio_hw_t* pio, int sm) {
+        hw = new DistanceSensor{pio, static_cast<uint>(sm), static_cast<uint>(trig)};
+        add_repeating_timer_ms(60, timer_callback, this, &timer);
+
+        // #if ENABLE_DEBUG
+        // printf("[Distance] Started: Trig GP%d, Echo GP%d (PIO%d, SM%d)\n", 
+        //         trig, echo, (pio == pio1), sm);
+        // #endif
+    }
+
+    bool changed() {
+        if (!hw) return false;
+        
+        int d = hw->distance;
+        if (std::abs(d - last_reported) > 1) {
+            last_reported = d;
+            current_val = static_cast<float>(d);
+
+            // #if ENABLE_DEBUG
+            // printf("[Distance] New Value: %0.1f cm\n", current_val);
+            // #endif
+
+            return true;
+        }
+        return false;
+    }
+
+    float getDistance() const { return current_val; }
+};
+
+inline SonicSensor dist_sensor;
+
+
+inline void addDistanceSensor(uint32_t trig, uint32_t echo) {
+    dist_sensor.init(trig, echo, pio1, 3);
+}
+
+inline bool processDistanceSensor() {
+    return dist_sensor.changed();
+}
+
+inline float getDistance() {
+    return dist_sensor.getDistance();
+}
+
+}
+
+#endif
